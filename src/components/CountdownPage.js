ead1d99b475f21560f352f0f1215386094fe9b5d
Rohit Bora <rohitbora178@gmail.com>
2026-03-28 16:07:59 +0530

    finish message page

--- src/components/CountdownPage.js
+++ src/components/CountdownPage.js
@@ -5,7 +5,7 @@ const CountdownPage = ({ countdown }) => (
     <div className="countdown-container">
       <h2 className="countdown-text">Get Ready!</h2>
       <div className="countdown-number">{countdown}</div>
-      <p className="countdown-message">Your special moment is here....</p>
+      <p className="countdown-message">Your special moment is here...</p>
     </div>
   </div>
 );
